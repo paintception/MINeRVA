09c2e9ae1e76069a8903176ded845ce2578e619c
paintception <matthia.sabatelli@gmail.com>
2020-03-23 10:18:41 +0100

    uodate general testing script

--- test/test_yolo.py
+++ test/test_yolo.py
@@ -1,12 +1,15 @@
 """
-Script which validates the performance of a YOLO model on a separate testing-set.
-We create for each image in the dataset a file in which for each image we store the predictions given by the model in
+Script which validates the performance of a trained YOLO model on a separate testing-set.
+We create for each image in the dataset a txt file where we store the predictions that are given by the model in
 the following format:
 
-    - detected_class_name x_min y_min x_max y_max
+    - detected_class_name x_min y_min x_max y_max coordinates
 
 This is important for computing the mAP once a similar file with the ground truth is created as presented in
-https://github.com/Cartucho/mAP and done in the pascal-VOCO-2012 challenge
+the ../prepare_datasets/ folder.
+
+When ran this script will automatically create two folders, one where the txt files will be stored and another one which
+will save the detected bounding boxes on top of the original image as presented in the README.md.
 """
 
 import os
@@ -14,12 +17,12 @@ import argparse
 from yolo import YOLO
 from PIL import Image
 
-DATASET_NAME = 'IconArt'
+DATASET_NAME = 'example_dataset'
 
-IMAGES = './testing_set_tiny_MIA/'
+IMAGES = '../testing_images/' #path to the images you would like to detect an instrument in
 
-DETECTION_RESULTS ='./yolo_detections/' + DATASET_NAME + '/'
-DETECTED_IMAGES = './Images/' + DATASET_NAME + '/'
+DETECTION_RESULTS ='./detected_files/' #path where to store the detected bounding-boxes
+DETECTED_IMAGES = './detected_images/' #path where to store an image with the detected bounding-box
 
 if not os.path.exists(DETECTED_IMAGES):
     os.makedirs(DETECTED_IMAGES)
@@ -28,21 +31,26 @@ if not os.path.exists(DETECTION_RESULTS):
     os.makedirs(DETECTION_RESULTS)
 
 def detect_img(yolo):
+
     for img in os.listdir(IMAGES):
-        base = os.path.basename(img)
-        tmp_filename = os.path.splitext(base)[0]
+            base = os.path.basename(img)
+            tmp_filename = os.path.splitext(base)[0]
 
-        image = Image.open(IMAGES + img)
-        results = yolo.detect_image(image)
-        filename = DETECTION_RESULTS + tmp_filename + '.txt'
+            image = Image.open(IMAGES + img)
+            results = yolo.detect_image(image)
+            filename = DETECTION_RESULTS + tmp_filename + '.txt'
 
-        with open(filename, 'w') as f:
-            for class_name, min_coord, max_coord in zip(*results[:3]):
-                f.write("%s " % class_name)
-                f.write("%s " % str(min_coord)[1:-1].replace(',',''))
-                f.write("%s \n" % str(max_coord)[1:-1].replace(',',''))
-
-        results[3].save(DETECTED_IMAGES + 'detections_' + tmp_filename + '.jpg')
+            with open(filename, 'w') as f:
+                if results is not None:
+                    for class_name, min_coord, max_coord in zip(*results[:3]):
+                        f.write("%s " % class_name)
+                        f.write("%s " % str(min_coord)[1:-1].replace(',',''))
+                        f.write("%s \n" % str(max_coord)[1:-1].replace(',',''))
+                    assert isinstance(results, object)
+                    results[3].save(DETECTED_IMAGES + 'detections_' + tmp_filename + '.jpg')
+                else:
+                    # it might be that there are no detections so we just store and empty txt file
+                    continue
 
     yolo.close_session()
 
