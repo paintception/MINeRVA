c7aefa8de028db341ec9472c646803cdc07128a4
Nicolae Banari <nicolae@Nicolaes-MacBook-Pro.local>
2020-03-26 15:14:23 +0100

    absolute path updated

--- classification_of_crops/cut_images.py
+++ classification_of_crops/cut_images.py
@@ -1,9 +1,19 @@
-
 import torchvision.transforms as transforms
-from PIL import Image, ImageDraw
+from PIL import Image
 from os.path import join as jp
 import os
 import pandas as pd
+import argparse
+
+
+def arguments():
+    parser = argparse.ArgumentParser(description='CharNMT arguments')
+
+    parser.add_argument('-data', type=str, help='path to data', default=None)
+    parser.add_argument('-splits', type=str, help='path to splits', default=None)
+    parser.add_argument('-save', type=str, help='path to save processed images', default=None)
+
+    return parser.parse_args()
 
 
 def square_image(im, size=(224, 224)):
@@ -25,19 +35,23 @@ def square_image(im, size=(224, 224)):
     return im
 
 
-def map_images(data_path, base, part, save_path, class_name='instrument_name', crop=True):
-    df = pd.read_csv(os.path.join(base, part + '.txt'))
+def map_images(data_path, splits_path, part, save_path, crop=True):
+    df = pd.read_csv(os.path.join(splits_path, part + '.txt'))
 
     for row in df.iterrows():
         file_name = row[1]['image_filename']
-        instrument = row[1][class_name]
+        if 'hypernym' in splits_path:
+            instrument = row[1]['lev1']
+        else:
+            instrument = row[1]['instrument_name']
         bb = row[1]['bounding_box_coordinates'].split(",")
 
         bb = list(map(int, bb))
         x_min, y_min, x_max, y_max = bb
 
         try:
-            img = Image.open(os.path.join(data_path, file_name.replace('%22', '"')))
+            # img = Image.open(os.path.join(data_path, file_name.replace('%22', '"')))
+            img = Image.open(os.path.join(data_path, file_name))
         except:
             print("The annotation is corrupted:", file_name)
             continue
@@ -55,20 +69,17 @@ def map_images(data_path, base, part, save_path, class_name='instrument_name', c
         class_path = jp(jp(save_path, part), instrument)
 
         if not os.path.isdir(class_path):
-            os.mkdir(class_path)
+            os.makedirs(class_path)
 
         cropped.save(jp(class_path, file_name))
 
 
 if __name__ == '__main__':
-
-    top = "source"
+    args = arguments()
 
     for p in ("train", "dev", "test"):
-        map_images(data_path="/home/nbanar/pycharmProjects/datasets/RawInstrumentsFullCollection",
-                   base=f"/home/nbanar/pycharmProjects/art_detector/classification_of_crops/splits/{top}/",
+        map_images(data_path=args.data,
+                   splits_path=args.splits,
                    part=p,
-                   save_path=f"/home/nbanar/pycharmProjects/art_detector/classification_of_crops/data/{top}/",
-                   class_name='instrument_name',
-                   crop=False)
-
+                   save_path=args.save,
+                   crop=True)
